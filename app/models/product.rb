cd15d3d869e72b5e8cf9f796c94fd83c9235b288
Catherine French <catherine.french@me.com>
2016-09-22 10:04:14 -0400

    Added partial for products view. Includes get_time_remaining function in product.rb.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :rewards
+
+  def get_time_remaining()
+    time_in_seconds = (self.end_date - self.start_date).to_i
+
+    days_float = (time_in_seconds/86400.0)
+    days_truncated = days_float.truncate
+
+    hours_float = (days_float - days_truncated)*24
+    hours_truncated = hours_float.truncate
+
+    minutes_float = (hours_float - hours_truncated)* 60
+    minutes_truncated = minutes_float.round
+    return "Hurry! There are only #{days_truncated} days #{hours_truncated} hours and #{minutes_truncated} minutes left in this campaign."
+  end
 end
