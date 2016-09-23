dc1a6de9131cd67516181c7a23e36f95cd372897
Thomas Spencer <t.spencer1990@hotmail.com>
2016-09-23 14:31:07 -0400

    Added pledge feature using ajax requests.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,7 +1,25 @@
 $(function() {
+  var input;
+  $('.reward-container').on('click', function(e) {
 
-  $('.reward-container').on('click', function() {
-    $('.reward-container').append("<%= escape_javascript render(:partial => 'pledges/form') %>");
+    $(this).unbind('click');
+
+    var me = $(this);
+    $('<lable>').attr('for', 'amount').html('Pledge Amount').appendTo(me);
+    input = $('<input>').attr('type', 'number').attr('name', 'rewardAmount').attr('id', me.attr('id')).appendTo(me)
+  });
+
+  $('.reward-container').on( 'focusout', 'input', function(){
+    input.prop('readonly', true);
+    var pledge = {reward_id: $(this).attr('id'), product_id: $('.product-field').attr('id'), amount: input.val()};
+
+
+    $.ajax({
+      url: "/pledges",
+      method: 'POST',
+      dataType: 'json',
+      data: {pledge: pledge}
+    });
   });
 
 });
